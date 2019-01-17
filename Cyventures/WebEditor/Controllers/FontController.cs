1fc4046c3e950733d478330eaaa3745bd47ca071
hakelimopu <hakelimopu@gmail.com>
2019-01-17 04:48:34 -0600

    db font editor working

--- Cyventures/WebEditor/Controllers/FontController.cs
+++ Cyventures/WebEditor/Controllers/FontController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +24,86 @@ namespace WebEditor.Controllers
             using (var db = new EFModel.TOWDEntities())
             {
                 return View(db.Fonts.Single(x=>x.FontId==id));
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EFModel.Font model)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var font = db.Fonts.Single(x => x.FontId == model.FontId);
+                font.FontName = model.FontName;
+                font.FontHeight = model.FontHeight;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Detail(int id)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var font = db.Fonts.Include("Glyphs").Single(x => x.FontId == id);
+                return View(font);
+            }
+        }
+        public ActionResult EditGlyph(int id)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var glyph = db.Glyphs.Include("Font").Include("GlyphPixels").Single(x => x.GlyphId == id);
+
+                return View(glyph);
+            }
+        }
+        public FileResult GlyphImage(int id, int scale=1)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var glyph = db.Glyphs.Include("Font").Include("GlyphPixels").Single(x => x.GlyphId == id);
+
+                Image img = new Bitmap(scale * glyph.GlyphWidth, scale*glyph.Font.FontHeight);
+                Graphics g = Graphics.FromImage(img);
+                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 0, scale * glyph.GlyphWidth, scale * glyph.Font.FontHeight));
+                var brush = new SolidBrush(Color.Black);
+
+                foreach(var glyphPixel in glyph.GlyphPixels)
+                {
+                    g.FillRectangle(brush, new Rectangle(glyphPixel.X * scale, glyphPixel.Y * scale, scale, scale));
+                }
+
+                MemoryStream ms = new MemoryStream();
+                img.Save(ms, ImageFormat.Png);
+
+                ms.Position = 0;
+
+                return new FileStreamResult(ms, "image/png");
+            }
+        }
+        public ActionResult RemoveGlyphPixel(int id)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var glyphPixel = db.GlyphPixels.Single(x => x.GlyphPixelId == id);
+                var glyphId = glyphPixel.GlyphId;
+                db.GlyphPixels.Remove(glyphPixel);
+                db.SaveChanges();
+                return RedirectToAction("EditGlyph", new { id = glyphId });
+            }
+        }
+        public ActionResult AddGlyphPixel(int glyphid, int x, int y)
+        {
+            using (var db = new EFModel.TOWDEntities())
+            {
+                var glyphPixel = new EFModel.GlyphPixel()
+                {
+                    GlyphId = glyphid,
+                    X = x,
+                    Y = y
+                };
+                db.GlyphPixels.Add(glyphPixel);
+                db.SaveChanges();
+                return RedirectToAction("EditGlyph", new { id = glyphid });
             }
         }
     }
