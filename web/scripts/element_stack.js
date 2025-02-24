36ba8780301f5f0860e74f103dbb9218c72270cd
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-02-24 04:47:53 -0600

    predicates and such

--- web/scripts/element_stack.js
+++ web/scripts/element_stack.js
@@ -20,58 +20,32 @@ class ElementStack {
     static cls() {
         ElementStack.clear(ElementStack.top());
     }
-    static add_button_to(parent, text, on_click) {
-        let button = document.createElement("button");
-        button.innerText = text;
-        button.addEventListener("click", on_click);
-        parent.appendChild(button);
-        return button;
+    static add_button(text, on_click, predicate) {
+        let element = ElementStack.add_element_to(ElementStack.top(), "button", predicate)
+        element.innerText = text;
+        element.addEventListener("click", on_click);
+        return element
     }
-    static add_button(text, on_click) {
-        return ElementStack.add_button_to(ElementStack.top(), text, on_click);
+    static add_paragraph(text, predicate) {
+        let element = ElementStack.add_element_to(ElementStack.top(), "p", predicate)
+        element.innerText = text;
+        return element
     }
-    static add_paragraph_to(parent, text) {
-        let p = document.createElement("p");
-        p.innerHTML = text;
-        parent.appendChild(p);
-        return p;
+    static add_span(text, predicate) {
+        let element = ElementStack.add_element_to(ElementStack.top(), "span", predicate)
+        element.innerText = text;
+        return element
     }
-    static add_paragraph(text) {
-        return ElementStack.add_paragraph_to(ElementStack.top(), text);
+    static add_break(predicate) {
+        return ElementStack.add_element_to(ElementStack.top(), "br", predicate)
     }
-    static add_span_to(parent, text) {
-        let p = document.createElement("span");
-        p.innerHTML = text;
-        parent.appendChild(p);
-        return p;
+    static add_img(img_url, predicate){
+        let element = ElementStack.add_element_to(ElementStack.top(), "img", predicate);
+        element.src = img_url;
+        return element;
     }
-    static add_span(text) {
-        return ElementStack.add_span_to(ElementStack.top(), text);
-    }
-    static add_break_to(parent) {
-        let br = document.createElement("br");
-        parent.appendChild(br);
-        return br;
-    }
-    static add_break() {
-        return ElementStack.add_break_to(ElementStack.top());
-    }
-    static add_img_to(parent, img_url) {
-        let img = document.createElement("img");
-        img.src = img_url;
-        parent.appendChild(img);
-        return img;
-    }
-    static add_img(img_url){
-        return ElementStack.add_img_to(ElementStack.top(), img_url);
-    }
-    static add_div_to(parent){
-        let div = document.createElement("div");
-        parent.appendChild(div);
-        return div;
-    }
-    static add_div(){
-        return ElementStack.add_div_to(ElementStack.top());
+    static add_div(predicate){
+        return ElementStack.add_element_to(ElementStack.top(), "div", predicate)
     }
     static add_element_to(parent, element_type, predicate){
         let element = document.createElement(element_type);
