b110e7bc4b865df0d55c00bf22a43ee576e11dae
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-03-05 05:45:11 -0600

    shippit and gitignore

--- shippit.sh
+++ shippit.sh
@@ -7,5 +7,3 @@ dotnet publish ./src/towd/towd.vbproj -o ./pub-mac -c Release --sc -p:PublishSin
 butler push pub-windows thegrumpygamedev/towd:windows
 butler push pub-linux thegrumpygamedev/towd:linux
 butler push pub-mac thegrumpygamedev/towd:mac
-git add -A
-git commit -m "shipped it!"
