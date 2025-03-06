c40457319e0d3d6bf5808970f7ae596c5866c843
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-03-06 05:48:49 -0600

    improved the shippit script

--- shippit.sh
+++ shippit.sh
@@ -4,6 +4,9 @@ rm -rf ./pub-mac
 dotnet publish ./src/towd/towd.vbproj -o ./pub-linux -c Release --sc -p:PublishSingleFile=true -r linux-x64
 dotnet publish ./src/towd/towd.vbproj -o ./pub-windows -c Release --sc -p:PublishSingleFile=true -r win-x64
 dotnet publish ./src/towd/towd.vbproj -o ./pub-mac -c Release --sc -p:PublishSingleFile=true -r osx-x64
+rm -f ./pub-linux/*.pdb
+rm -f ./pub-windows/*.pdb
+rm -f ./pub-mac/*.pdb
 butler push pub-windows thegrumpygamedev/towd:windows
 butler push pub-linux thegrumpygamedev/towd:linux
 butler push pub-mac thegrumpygamedev/towd:mac
