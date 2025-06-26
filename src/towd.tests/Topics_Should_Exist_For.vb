305318618495a39aff64a45cf64ec3d54dfdcdfd
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-06-26 04:17:07 -0500

    topics for recipes

---
+++ src/towd.tests/Topics_Should_Exist_For.vb
@@ -0,0 +1,16 @@
+﻿Imports Shouldly
+Imports towd.business
+Imports Xunit
+
+Public Class Topics_Should_Exist_For
+    <Fact>
+    Public Sub Recipe_Types()
+        For Each sut In [Enum].GetValues(Of RecipeType)
+            Try
+                Topics.RecipeTypeTopicTable.ContainsKey(sut).ShouldBeTrue($"{sut} Should Have a Topic")
+            Catch ex As Exception
+                Assert.Fail($"{sut} Should Have a Topic")
+            End Try
+        Next
+    End Sub
+End Class
