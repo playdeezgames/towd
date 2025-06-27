157903079e5660f3fa740a293decab9ceea67163
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-06-27 05:13:27 -0500

    item type tests

--- src/towd.business.tests/Enumeration_Values_Should_Have_Descriptors_For.vb
+++ src/towd.business.tests/Enumeration_Values_Should_Have_Descriptors_For.vb
@@ -1,10 +1,19 @@
-Imports System.Reflection.Metadata.Ecma335
 Imports Shouldly
 Imports towd.data
 Imports Xunit
 
 Namespace towd.business.tests
     Public Class Enumeration_Values_Should_Have_Descriptors_For
+        <Fact>
+        Sub Item_Types()
+            For Each sut In [Enum].GetValues(Of ItemType)
+                Try
+                    sut.ToDescriptor.ShouldNotBeNull($"{sut}'s Descriptor Should Not Be Null")
+                Catch ex As Exception
+                    Assert.Fail($"{sut}'s Descriptor Should Exist")
+                End Try
+            Next
+        End Sub
         <Fact>
         Sub Verb_Types()
             For Each sut In [Enum].GetValues(Of VerbType)
