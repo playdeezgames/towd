11a9d02f98e6020be153ed7968d7be058cf78ac4
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-06 04:16:10 -0500

    all on deeds screen

--- src/towd/InPlay/DeedsState.vb
+++ src/towd/InPlay/DeedsState.vb
@@ -4,6 +4,7 @@ Friend Class DeedsState
     Inherits ChildView
     Private ReadOnly availableListView As ListView
     Private ReadOnly doneListView As ListView
+    Private ReadOnly allListView As ListView
     Public Sub New(mainView As MainView)
         MyBase.New(mainView)
         Dim titleLabel As New Label With
@@ -25,11 +26,17 @@ Friend Class DeedsState
                 .Height = [Dim].Fill
             }
         AddHandler doneListView.OpenSelectedItem, AddressOf OnDoneListViewOpenSelectedItem
+        allListView = New ListView With
+            {
+                .Width = [Dim].Fill,
+                .Height = [Dim].Fill
+            }
+        AddHandler allListView.OpenSelectedItem, AddressOf OnAllListViewOpenSelectedItem
         Dim tabView As New TabView With
             {
                 .Y = Pos.Bottom(titleLabel),
                 .Width = [Dim].Fill,
-                .Height = [Dim].Fill - 3
+                .Height = [Dim].Fill - 1
             }
         Dim availableTab As New TabView.Tab With
             {
@@ -41,18 +48,29 @@ Friend Class DeedsState
                 .Text = "Done",
                 .View = doneListView
             }
+        Dim allTab As New TabView.Tab With
+            {
+                .Text = "All",
+                .View = allListView
+            }
         tabView.AddTab(availableTab, False)
         tabView.AddTab(doneTab, False)
+        tabView.AddTab(allTab, False)
         Add(tabView)
 
         Dim closeButton As New Button("Close") With
             {
                 .X = Pos.Center,
-                .Y = Pos.Bottom(tabView) + 1
+                .Y = Pos.Bottom(tabView)
             }
         AddHandler closeButton.Clicked, AddressOf CloseWindow
         Add(closeButton)
 
+    End Sub
+
+    Private Sub OnAllListViewOpenSelectedItem(args As ListViewItemEventArgs)
+        Dim deed = CType(args.Value, IDeed)
+        MessageBox.Query(deed.ToString, deed.Description, "Ok")
     End Sub
 
     Private Sub OnDoneListViewOpenSelectedItem(args As ListViewItemEventArgs)
@@ -79,8 +97,10 @@ Friend Class DeedsState
     Friend Overrides Sub UpdateView()
         Dim available As New List(Of IDeed)
         Dim done As New List(Of IDeed)
+        Dim all As New List(Of IDeed)
         Dim character = World.Avatar
         For Each descriptor In Deeds.Descriptors.Values.OrderBy(Function(x) x.Name)
+            all.Add(descriptor)
             If character.HasDone(descriptor) Then
                 done.Add(descriptor)
             ElseIf character.IsAvailable(descriptor) Then
@@ -89,5 +109,6 @@ Friend Class DeedsState
         Next
         availableListView.SetSource(available)
         doneListView.SetSource(done)
+        allListView.SetSource(all)
     End Sub
 End Class
