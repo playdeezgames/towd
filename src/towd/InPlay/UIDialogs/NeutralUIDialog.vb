60f68ff6db10e12ca68c4bb40c6f6f52a117ceb1
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-13 04:53:03 -0500

    added dialog, about to get rid of state

---
+++ src/towd/InPlay/UIDialogs/NeutralUIDialog.vb
@@ -0,0 +1,11 @@
+﻿Friend Module NeutralUIDialog
+    Function DetermineInPlayDialog(context As IUIContext) As (String, IUIDialog)
+        context.SaveGame(SaveSlot.Auto, Sub() Return)
+        Dim character = context.World.Avatar
+        If character.IsDead Then
+            Return (GameState.Dead, Nothing)
+        Else
+            Return (GameState.Navigation, Nothing)
+        End If
+    End Function
+End Module
