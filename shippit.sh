21590f5a7c069ad10a4e352018bdf58539eb018d
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-15 15:25:41 -0500

    i now ship both terminal gui as well as spectre console.

--- shippit.sh
+++ shippit.sh
@@ -4,6 +4,9 @@ rm -rf ./pub-mac
 dotnet publish ./src/towd/towd.vbproj -o ./pub-linux -c Release --sc -p:PublishSingleFile=true -r linux-x64
 dotnet publish ./src/towd/towd.vbproj -o ./pub-windows -c Release --sc -p:PublishSingleFile=true -r win-x64
 dotnet publish ./src/towd/towd.vbproj -o ./pub-mac -c Release --sc -p:PublishSingleFile=true -r osx-x64
+dotnet publish ./src/towd.spectre/towd.spectre.vbproj -o ./pub-linux -c Release --sc -p:PublishSingleFile=true -r linux-x64
+dotnet publish ./src/towd.spectre/towd.spectre.vbproj -o ./pub-windows -c Release --sc -p:PublishSingleFile=true -r win-x64
+dotnet publish ./src/towd.spectre/towd.spectre.vbproj -o ./pub-mac -c Release --sc -p:PublishSingleFile=true -r osx-x64
 rm -f ./pub-linux/*.pdb
 rm -f ./pub-windows/*.pdb
 rm -f ./pub-mac/*.pdb
