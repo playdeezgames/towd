8e50026104a9bfa6999f4df4d4f072e2882d688c
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-16 08:50:22 -0500

    put towd into the caption of the console app

--- src/towd.spectre/Program.vb
+++ src/towd.spectre/Program.vb
@@ -4,6 +4,7 @@ Imports towd.ui
 
 Module Program
     Sub Main(args As String())
+        Console.Title = "TOWD"
         Dim context As IUIContext(Of IWorld) = New UIContext
         While Not context.IsClosed
             AnsiConsole.Clear()
