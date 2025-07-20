db39c08f7189372b99b1897ce9c95bec38f45173
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-20 04:59:11 -0500

    verbs menu is now also available verbs menu

--- src/towd.ui/Dialogs/InPlay/Verbs/VerbMenuUIDialog.vb
+++ src/towd.ui/Dialogs/InPlay/Verbs/VerbMenuUIDialog.vb
@@ -6,8 +6,8 @@ Friend Class VerbMenuUIDialog
     Private ReadOnly context As IUIContext(Of IWorld)
     Private ReadOnly cancelDialog As Func(Of IUIDialog)
     Const NEVER_MIND_TEXT = "Never Mind"
-    Const AVAILABLE_TEXT = "Available"
-    Const ALL_TEXT = "All"
+    Const ALL_VERBS_TEXT = "All Verbs..."
+    Private table As Dictionary(Of String, Func(Of IUIDialog)) = Nothing
 
     Public Function GetLinesAsync() As Task(Of IEnumerable(Of (Mood As String, Text As String, EndsLine As Boolean))) Implements IUIDialog.GetLinesAsync
         Return Task.FromResult(Of IEnumerable(Of (Mood As String, Text As String, EndsLine As Boolean)))(Array.Empty(Of (String, String, Boolean)))
@@ -17,10 +17,44 @@ Friend Class VerbMenuUIDialog
         Dim result As New List(Of String) From
                 {
                     NEVER_MIND_TEXT,
-                    AVAILABLE_TEXT,
-                    ALL_TEXT
+                    ALL_VERBS_TEXT
                 }
+        If table Is Nothing Then
+            table = VerbTypes.Descriptors.Values.
+            Where(AddressOf VerbTypeFilter).
+            GroupBy(Function(x) x.VerbCategoryType).
+            ToDictionary(
+                AddressOf getTableKey,
+                AddressOf getTableValue)
+        End If
+        result.AddRange(table.Keys)
         Return Task.FromResult(Of IEnumerable(Of String))(result)
+    End Function
+
+    Private Function getTableValue(grouping As IGrouping(Of String, IVerbType)) As Func(Of IUIDialog)
+        If grouping.Count = 1 Then
+            Return Function() As IUIDialog
+                       Return New VerbDetailUIDialog(context, grouping.Single, False, Function() New VerbMenuUIDialog(context, cancelDialog))
+                   End Function
+        End If
+        Return Function() As IUIDialog
+                   Return New FilteredVerbCategoryUIDialog(context, grouping.Key.ToVerbCategoryDescriptor.Name, grouping.Key, AddressOf ActualVerbTypeFilter, Function() New VerbMenuUIDialog(context, cancelDialog))
+               End Function
+    End Function
+
+    Private Function ActualVerbTypeFilter(type As IVerbType, character As ICharacter) As Boolean
+        Return type.CanPerform(character)
+    End Function
+
+    Private Function VerbTypeFilter(type As IVerbType) As Boolean
+        Return ActualVerbTypeFilter(type, context.World.Avatar)
+    End Function
+
+    Private Function getTableKey(grouping As IGrouping(Of String, IVerbType)) As String
+        If grouping.Count = 1 Then
+            Return grouping.Single.Name
+        End If
+        Return $"{grouping.Key.ToVerbCategoryDescriptor.Name}..."
     End Function
 
     Public Function GetPromptAsync() As Task(Of String) Implements IUIDialog.GetPromptAsync
@@ -34,10 +68,8 @@ Friend Class VerbMenuUIDialog
 
     Public Function Choose(choice As String) As Task(Of IUIDialog) Implements IUIDialog.Choose
         Select Case choice
-            Case ALL_TEXT
+            Case ALL_VERBS_TEXT
                 Return Task.FromResult(Of IUIDialog)(New FilteredVerbUIDialog(context, "All Verbs", Function(verbType As IVerbType, character As ICharacter) True, Function() Me))
-            Case AVAILABLE_TEXT
-                Return Task.FromResult(Of IUIDialog)(New FilteredVerbUIDialog(context, "Available Verbs", Function(verbType As IVerbType, character As ICharacter) verbType.CanPerform(character), Function() Me))
             Case NEVER_MIND_TEXT
                 Return Task.FromResult(cancelDialog())
             Case Else
