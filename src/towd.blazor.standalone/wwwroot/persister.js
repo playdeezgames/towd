8c8c513c061ddd843cf349be4535b0495004651e
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-07-20 04:06:14 -0500

    i fully have web based saving and loading working, curse the existence of async and await.

--- src/towd.blazor.standalone/wwwroot/persister.js
+++ src/towd.blazor.standalone/wwwroot/persister.js
@@ -1,16 +1,26 @@
 ﻿function loadGame(filename) {
-    let saveData = localStorage.getItem(filename)
-    if (saveData == null) {
+    try {
+        let saveData = localStorage.getItem(filename)
+        if (saveData == null) {
+            return null;
+        }
+        return JSON.parse(saveData).what;
+    } catch (ex) {
+        console.log(ex);
         return null;
     }
-    return JSON.parse(saveData).what;
 }
 function saveExists(filename) {
-    let saveData = localStorage.getItem(filename)
-    if (saveData == null) {
+    try {
+        let saveData = localStorage.getItem(filename)
+        if (saveData == null) {
+            return null;
+        }
+        return JSON.parse(saveData).when;
+    } catch (ex) {
+        console.log(ex);
         return null;
     }
-    return JSON.parse(saveData).when;
 }
 function saveGame(filename, data) {
     let saveData = { "when": new Date(), "what": data };
